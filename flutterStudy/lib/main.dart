303f7b7a327486154a0ec8a9f67592d0eb050c3f
kkimwooo <kkimwooo@gmail.com>
2021-07-20 16:16:09 +0900

    flutterStudy는 책에서 이론 공부한 내용
    실습 예제를 만들기 위해 폴더 분리함

---
+++ flutterStudy/lib/main.dart
@@ -0,0 +1,247 @@
+import 'package:flutter/material.dart'; // Material Design Widget 포함한 패키지
+import 'package:carousel_slider/carousel_slider.dart';
+
+//앱 시작 부분, 특별히 수정할 일이 없음
+void main() => runApp(MyApp());
+
+//시작 클래스. Material Design App 생성, StatelessWidget 상태를 가지지 않는 위젯
+class MyApp extends StatelessWidget {
+  //StatelessWidget은 build() 메서드를 가지고 있음
+  @override
+  Widget build(BuildContext context) {
+    //StatelessWidget을 상속받은 클래스는 MaterialApp 클래스 인스턴스를 반환
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var _index = 0;
+  var _pages = [
+    Page1(),
+    Page2(),
+    Page3(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          '복잡한 UI',
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.add,
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: _pages[_index],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) {
+          setState(() {
+            _index = index;
+          });
+          print(index);
+        },
+        currentIndex: _index,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(label: '홈', icon: Icon(Icons.home)),
+          BottomNavigationBarItem(label: '이용서비스', icon: Icon(Icons.assignment)),
+          BottomNavigationBarItem(
+              label: '내 정보', icon: Icon(Icons.account_circle)),
+        ],
+      ),
+    );
+  }
+}
+
+class Page1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        _buildTop(),
+        _buildMiddle(),
+        _buildBottom(),
+      ],
+    );
+  }
+
+  //상단
+  Widget _buildTop() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20, bottom: 20),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              GestureDetector(
+                onTap: () {
+                  print('ok');
+                },
+                child: Column(
+                  children: <Widget>[
+                    Icon(
+                      Icons.local_taxi,
+                      size: 40,
+                    ),
+                    Text('택시'),
+                  ],
+                ),
+              ),
+              Column(
+                children: <Widget>[
+                  Icon(
+                    Icons.local_taxi,
+                    size: 40,
+                  ),
+                  Text('택시'),
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Icon(
+                    Icons.local_taxi,
+                    size: 40,
+                  ),
+                  Text('택시'),
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Icon(
+                    Icons.local_taxi,
+                    size: 40,
+                  ),
+                  Text('택시'),
+                ],
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 20, //20만큼의 여백을 표현
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                children: <Widget>[
+                  Icon(
+                    Icons.local_taxi,
+                    size: 40,
+                  ),
+                  Text('택시'),
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Icon(
+                    Icons.local_taxi,
+                    size: 40,
+                  ),
+                  Text('택시'),
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Icon(
+                    Icons.local_taxi,
+                    size: 40,
+                  ),
+                  Text('택시'),
+                ],
+              ),
+              Opacity(
+                //Opacity 로 감싸면 위젯 숨기기 표시 안되게, html hidden과 비슷한듯
+                opacity: 0.0,
+                child: Column(
+                  children: <Widget>[
+                    Icon(
+                      Icons.local_taxi,
+                      size: 40,
+                    ),
+                    Text('택시'),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  //중단
+  Widget _buildMiddle() {
+    return CarouselSlider(
+      items: [1, 2, 3, 4, 5].map((e) {
+        return Builder(builder: (BuildContext context) {
+          return Container(
+              width: MediaQuery.of(context).size.width,
+              margin: EdgeInsets.symmetric(horizontal: 5.0),
+              decoration: BoxDecoration(color: Colors.amber),
+              child: Text(
+                'text $e',
+                style: TextStyle(fontSize: 16.0),
+              ));
+        });
+      }).toList(),
+      options: CarouselOptions(height: 150.0, autoPlay: true),
+    );
+  }
+
+  //하단
+  Widget _buildBottom() {
+    final items = List.generate(10, (index) {
+      return ListTile(
+        leading: Icon(Icons.notifications_none),
+        title: Text('$index번째 공지사항 입니다.'),
+      );
+    });
+    return ListView(
+      physics: NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      children: items,
+    );
+  }
+}
+
+class Page2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        '이용 서비스',
+        style: TextStyle(fontSize: 40),
+      ),
+    );
+  }
+}
+
+class Page3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        '내 정보',
+        style: TextStyle(fontSize: 40),
+      ),
+    );
+  }
+}
